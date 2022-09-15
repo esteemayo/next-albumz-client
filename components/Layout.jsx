a7c86d271c2a3539b4222e239330019229c6d6db
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-15 18:24:27 +0100

    added sidebar component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       {children}
       <Footer />
     </div>
