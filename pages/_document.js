8c7430cb977b7db7849fe885734895d5ea9c917f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-20 14:32:03 +0100

    added google font

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap' rel='stylesheet' />
         </Head>
         <body>
