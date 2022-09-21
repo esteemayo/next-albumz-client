bbe77a8a56aba7c7329b85c904de6e304ab30afe
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-21 20:04:15 +0100

    modified

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
 import Hero from '@/components/Hero';
+import Modal from '@/components/Modal';
+import AddButton from '@/components/AddButton';
+import AlbumForm from '@/components/AlbumForm';
+import GenreForm from '@/components/GenreForm';
 import TopReviews from '@/components/TopReviews';
 import FeaturedAlbums from '@/components/FeaturedAlbums';
 import styles from '@/styles/Home.module.scss';
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       <Hero />
       <FeaturedAlbums />
       <TopReviews />
+      <AddButton text='New album' onClick={() => setShowModal(true)} />
+      {showModal && (
+        <Modal onClose={setShowModal}>
+          {/* <AlbumForm /> */}
+          <GenreForm />
+        </Modal>
+      )}
     </>
   );
 };
