4c3070e046ef888797e72ffcfaa29c7a6131577f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-22 18:53:09 +0100

    refactored and imported AlbumCard component

--- pages/albums/index.jsx
+++ pages/albums/index.jsx
@@ -1,5 +1,32 @@
-const Album = () => {
-  return <div>Album</div>;
+import { useState } from 'react';
+
+import Modal from '@/components/Modal';
+import AddButton from '@/components/AddButton';
+import AlbumCard from '@/components/AlbumCard';
+import AlbumForm from '@/components/AlbumForm';
+import styles from '@/styles/Albums.module.scss';
+
+const Albums = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <AlbumCard />
+        <AlbumCard />
+        <AlbumCard />
+        <AlbumCard />
+        <AlbumCard />
+        <AlbumCard />
+      </div>
+      <AddButton text='New album' onClick={() => setShowModal(true)} />
+      {showModal && (
+        <Modal onClose={setShowModal}>
+          <AlbumForm />
+        </Modal>
+      )}
+    </main>
+  );
 };
 
-export default Album;
+export default Albums;
