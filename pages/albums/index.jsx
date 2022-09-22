9530c9ee8e2eb348d336ca1ad36d4df928e4aeef
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-22 20:56:53 +0100

    added pagination component

--- pages/albums/index.jsx
+++ pages/albums/index.jsx
@@ -4,6 +4,7 @@ import Modal from '@/components/Modal';
 import AddButton from '@/components/AddButton';
 import AlbumCard from '@/components/AlbumCard';
 import AlbumForm from '@/components/AlbumForm';
+import Pagination from '@/components/Pagination';
 import styles from '@/styles/Albums.module.scss';
 
 const Albums = () => {
@@ -19,6 +20,8 @@ const Albums = () => {
         <AlbumCard />
         <AlbumCard />
       </div>
+      <Pagination />
+      
       <AddButton text='New album' onClick={() => setShowModal(true)} />
       {showModal && (
         <Modal onClose={setShowModal}>
