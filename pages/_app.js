e1533e0ba736a774f81328a7ece1378befd4981c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-24 21:03:09 +0100

    fixed a bug

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </Provider>
   );
