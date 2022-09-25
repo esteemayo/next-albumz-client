d8718319ecaa1be0eb99a1b4df2314f4e373e2fc
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-25 18:27:01 +0100

    added tooltip to bookmark icon

--- components/AlbumDescription.jsx
+++ components/AlbumDescription.jsx
@@ -1,3 +1,19 @@
+import Zoom from '@mui/material/Zoom';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import AlbumOutlinedIcon from '@mui/icons-material/AlbumOutlined';
+import TitleOutlinedIcon from '@mui/icons-material/TitleOutlined';
+import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
+import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
+import MusicNoteOutlinedIcon from '@mui/icons-material/MusicNoteOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
+import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
+import BookmarkAddedOutlinedIcon from '@mui/icons-material/BookmarkAddedOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FormatListNumberedOutlinedIcon from '@mui/icons-material/FormatListNumberedOutlined';
+
+import StarRating from '@/components/StarRating';
 import styles from '@/styles/AlbumDescription.module.scss';
 
 const AlbumDescription = () => {
@@ -7,39 +23,76 @@ const AlbumDescription = () => {
         <div className={styles.album__wrapper}>
           <h2 className={styles.album__heading}>About album</h2>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <MusicNoteOutlinedIcon className={styles.icon} />
+            </span>
             <span>Artist</span>
             <span>Wizkid</span>
           </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <TitleOutlinedIcon className={styles.icon} />
+            </span>
             <span>Title</span>
             <span>Made in lagos</span>
             </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <CategoryOutlinedIcon className={styles.icon} />
+            </span>
             <span>Genre</span>
             <span>Afro pop</span>
           </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <DateRangeOutlinedIcon className={styles.icon} />
+            </span>
             <span>Year of Release</span>
             <span>2020</span>
           </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <AlbumOutlinedIcon className={styles.icon} />
+            </span>
             <span>Record Label</span>
             <span>Star boy</span>
           </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <FormatListNumberedOutlinedIcon className={styles.icon} />
+            </span>
             <span>Number of Tracks</span>
             <span>18</span>
           </div>
           <div className={styles.album__details}>
-            <span>Icon</span>
+            <span>
+              <StarOutlineOutlinedIcon className={styles.icon} />
+            </span>
             <span>Ratings</span>
-            <span>5.0</span>
+            <span>5 / 5</span>
+          </div>
+          <StarRating value={5} className={styles.rating} />
+          <div className={styles.action}>
+            <span className={styles.action__wrapper}>
+              <div className={styles.view__container}>
+                <VisibilityOutlinedIcon className={styles.action__icon} />
+                <span className={styles.views}>700 views</span>
+              </div>
+            </span>
+            <span className={styles.action__wrapper}>
+            <Tooltip TransitionComponent={Zoom} title='Bookmark' arrow>
+              <IconButton>
+                <BookmarkAddOutlinedIcon
+                  className={`${styles.action__icon} ${styles.bookmark__icon}`}
+                />
+              </IconButton>
+            </Tooltip>
+            </span>
+            <span className={styles.action__wrapper}>
+              <FavoriteBorderOutlinedIcon
+                className={`${styles.action__icon} ${styles.like__icon}`}
+              />
+            </span>
           </div>
         </div>
       </div>
