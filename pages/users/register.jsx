e391394c3b09ca5c08dae4dad0ed96270e5a80c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-26 16:51:16 +0100

    added icon and show/hide password

--- pages/users/register.jsx
+++ pages/users/register.jsx
@@ -1,17 +1,76 @@
 import Link from 'next/link';
+import { useState } from 'react';
+import ChipInput from 'material-ui-chip-input';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import Meta from '@/components/Meta';
 import styles from '@/styles/Login.module.scss';
 
+const initialState = {
+  name: '',
+  username: '',
+  email: '',
+  location: '',
+  favGenres: [],
+  favArtists: [],
+  password: '',
+  confirmPassword: '',
+  showPassword: false,
+};
+
 const Register = () => {
+  const [file, setFile] = useState(null);
+  const [formInputs, setFormInputs] = useState(initialState);
+
+  const handleShowPassword = () => {
+    setFormInputs((prev) => ({ ...prev, showPassword: !prev.showPassword }));
+  };
+
+  const handleChange = ({ target: input }) => {
+    const { name, value } = input;
+    setFormInputs((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleAddFavArtist = (favArtist) => {
+    setFormInputs((prev) => ({
+      ...prev,
+      favArtists: [...prev.favArtists, favArtist],
+    }));
+  };
+
+  const handleDeleteFavArtist = (favArtist) => {
+    setFormInputs((prev) => ({
+      ...prev,
+      favArtists: prev.favArtists.filter((artist) => artist !== favArtist),
+    }));
+  };
+  
+  const handleAddFavGenre = (favGenre) => {
+    setFormInputs((prev) => ({
+      ...prev,
+      favGenres: [...prev.favGenres, favGenre],
+    }));
+  };
+
+  const handleDeleteFavGenre = (favGenre) => {
+    setFormInputs((prev) => ({
+      ...prev,
+      favGenres: prev.favGenres.filter((genre) => genre !== favGenre),
+    }));
+  };
+
+  const {favGenres, favArtists, showPassword } = formInputs;
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log({ ...formInputs });
   };
 
   return (
     <>
       <Meta title='Create an Account - Albumz Music Entertainment' />
-      <div className={styles.container}>
+      <div className={`${styles.container} ${styles.registerContainer}`}>
         <div className={styles.wrapper}>
           <header className={styles.header}>
             <h1>User Registration</h1>
@@ -21,31 +80,109 @@ const Register = () => {
               <div className={styles.form__wrapper}>
                 <div className={styles.form__headline}>Create an Account</div>
                 <div className={styles.form__group}>
-                  <input type='text' placeholder='Name' className={styles.form__input} />
+                  <input
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                  />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='text' placeholder='Username' className={styles.form__input} />
+                  <input
+                    type='text'
+                    name='username'
+                    placeholder='Username'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                  />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='email' placeholder='Email Address' className={styles.form__input} />
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Email Address'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                  />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='text' placeholder='Location' className={styles.form__input} />
+                  <input
+                    type='text'
+                    name='location'
+                    placeholder='Location'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                  />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='text' placeholder='Favorite Genres' className={styles.form__input} />
+                  <ChipInput
+                      fullWidth
+                      name='favGenres'
+                      variant='outlined'
+                      placeholder='Favorite Genres'
+                      value={favGenres}
+                      onAdd={(genre) => handleAddFavGenre(genre)}
+                      onDelete={(genre) => handleDeleteFavGenre(genre)}
+                    />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='text' placeholder='Favorite Artists' className={styles.form__input} />
+                  <ChipInput
+                    fullWidth
+                    name='favArtists'
+                    variant='outlined'
+                    placeholder='Favorite Artists'
+                    value={favArtists}
+                    onAdd={(artist) => handleAddFavArtist(artist)}
+                    onDelete={(artist) => handleDeleteFavArtist(artist)}
+                  />
                 </div>
                 <div className={styles.form__group}>
-                  <input type='password' placeholder='Password' className={styles.form__input} />
+                  <input
+                    name='password'
+                    type={showPassword ? 'text': 'password'}
+                    placeholder='Password'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                  />
+                  {showPassword ? (
+                     <VisibilityOff
+                      onClick={handleShowPassword}
+                      className={styles.password__icon}
+                      />
+                   ) : (
+                    <Visibility
+                      onClick={handleShowPassword}
+                      className={styles.password__icon}
+                    />
+                   )}
                 </div>
                 <div className={styles.form__group}>
-                  <input type='password' placeholder='Confirm Password' className={styles.form__input} />
+                  <input
+                    name='confirmPassword'
+                    type={showPassword ? 'text': 'password'}
+                    placeholder='Confirm Password'
+                    onChange={handleChange}
+                    className={styles.form__input}
+                    />
+                    {showPassword ? (
+                     <VisibilityOff
+                      onClick={handleShowPassword}
+                      className={styles.password__icon}
+                      />
+                   ) : (
+                    <Visibility
+                      onClick={handleShowPassword}
+                      className={styles.password__icon}
+                    />
+                   )}
                 </div>
                 <div className={styles.form__group}>
-                  <input type='file' className={styles.form__input} />
+                  <input
+                  type='file'
+                  className={styles.form__input}
+                  onChange={(e) => setFile(e.target.files[0])}
+                  />
                 </div>
                 <div className={styles.form__btnWrapper}>
                   <button type='submit' className={styles.form__btn}>Register</button>
