ae8c7f60c18ffae80d6c6d3fda40eb3e145310ce
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-26 20:13:33 +0100

    first commit

---
+++ pages/albums/tags.jsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Tags.module.scss';
+
+const Tags = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Tags</h1>
+    </section>
+  );
+};
+
+export default Tags;
---
+++ pages/albums/top.jsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/TopAlbum.module.scss';
+
+const TopAlbums = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Top 10 Albums</h1>
+    </section>
+  );
+};
+
+export default TopAlbums;
