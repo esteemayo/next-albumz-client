94159bcb14affe852a23d1706e44590e0213e4b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-27 01:29:40 +0100

    created tags page and added tag card component

--- pages/albums/tags.jsx
+++ pages/albums/tags.jsx
@@ -1,11 +1,18 @@
+import TagCard from '@/components/TagCard';
 import styles from '@/styles/Tags.module.scss';
 
-const Tags = () => {
+const AlbumTags = () => {
   return (
     <section className={styles.container}>
-      <h1>Tags</h1>
+      <h1 className={styles.heading}>
+        <span className={styles.heading__main}>Album with tags:</span>
+        <span className={styles.heading__sub}>#hottest</span>
+      </h1>
+      <TagCard />
+      <TagCard />
+      <TagCard />
     </section>
   );
 };
 
-export default Tags;
+export default AlbumTags;
