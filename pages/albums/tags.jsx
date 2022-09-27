bcbcb96caef45bbffa23f0e659338c3fd2c69a24
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-27 22:10:41 +0100

    added hashtag icon component

--- pages/albums/tags.jsx
+++ pages/albums/tags.jsx
@@ -8,7 +8,9 @@ const AlbumTags = () => {
     <section className={styles.container}>
       <h1 className={styles.heading}>
         <span className={styles.heading__main}>Album with tags:</span>
-        <span className={styles.heading__sub}>#hottest</span>
+        <span className={styles.heading__sub}>
+          <TagOutlinedIcon className={styles.tagIcon}/>hottest
+        </span>
       </h1>
       <TagCard />
       <TagCard />
