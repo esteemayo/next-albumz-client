fcb55b8b72f1a3d59c3b39b272b0e5ad1ffe90f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-28 03:12:00 +0100

    fixed a bug

--- components/FormChipInput.jsx
+++ components/FormChipInput.jsx
@@ -1,15 +1,18 @@
 import ChipInput from 'material-ui-chip-input';
+import styles from '@/styles/FormChipInput.module.scss';
 
 const FormChipInput = ({ name, value, placeholder, ...rest }) => {
   return (
-    <ChipInput
-      {...rest}
-      fullWidth
-      name={name}
-      value={value}
-      variant='outlined'
-      placeholder={placeholder}
-    />
+    <div className={styles.form__group}>
+      <ChipInput
+        {...rest}
+        fullWidth
+        name={name}
+        value={value}
+        variant='outlined'
+        placeholder={placeholder}
+      />
+    </div>
   );
 };
 
