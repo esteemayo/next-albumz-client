0875c36f1b77f48c79a45e9f092868040ab26588
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-09-30 03:58:51 +0100

    created delete account modal contents

---
+++ components/DeleteAccount.jsx
@@ -0,0 +1,36 @@
+import styles from '@/styles/DeleteAccount.module.scss';
+
+const DeleteAccount = ({ onClose }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.warning}>
+          <h4>You are about to delete your account</h4>
+          <p>All your albums will be permanently removed and you won't be 
+            able to see them again, including the ones you have shared with 
+            your friends.
+          </p>
+        </div>
+        <div className={styles.btnContainer}>
+          <button
+            onClick={() => onClose(true)}
+            className={styles.cancelBtn}
+          >
+            Cancel
+          </button>
+          <button
+          onClick={() => {
+            console.log('Account closed');
+            onClose(true);
+          }}
+          className={styles.deleteBtn}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteAccount;
