15db5e14781209e283dec3af4b6ee0b586d50836
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-02 03:28:05 +0100

    created ui for the component

--- components/RelatedAlbums.jsx
+++ components/RelatedAlbums.jsx
@@ -1,8 +1,18 @@
+import RelatedAlbumCard from './RelatedAlbumCard';
 import styles from '@/styles/RelatedAlbums.module.scss';
 
 const RelatedAlbums = () => {
   return (
-    <section></section>
+    <section className={styles.related}>
+      <div className={styles.related__wrapper}>
+        <h6 className={styles.related__header}>Related Albums</h6>
+        <div className={styles.related__container}>
+          <RelatedAlbumCard />
+          <RelatedAlbumCard />
+          <RelatedAlbumCard />
+        </div>
+      </div>
+    </section>
   );
 };
 
