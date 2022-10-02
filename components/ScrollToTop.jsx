d9180b3ed1b9888ab18916781539e736dd8dc2c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-02 18:19:02 +0100

    added scroll functionalities

--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+
 import styles from '@/styles/ScrollToTop.module.scss';
 
 const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  if(typeof window !== 'undefined') {
+    window.onscroll = ()=> {
+      setIsVisible(window.pageYOffset > 300 ? true : false);
+      return () => (window.onscroll = null);
+    };
+  }
+
+  const handleScroll = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
   return (
     <div className={styles.scroll}>
-      <div className={styles.iconContainer}>
-        <KeyboardArrowUpOutlinedIcon />
-      </div>
+      {isVisible && (
+        <div onClick={handleScroll} className={styles.iconContainer}>
+          <KeyboardArrowUpOutlinedIcon />
+        </div>
+      )}
     </div>
   );
 };
