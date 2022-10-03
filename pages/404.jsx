90666b18776e900f35bfd762fe99d489851a848b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-03 01:31:39 +0100

    designed the page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,11 +1,20 @@
+import Image from 'next/image';
+
 import Meta from '@/components/Meta';
+import styles from '@/styles/404.module.scss';
 
 const NotFound = () => {
   return (
     <>
       <Meta title='Page Not Found - Albumz Music Entertainment' />
-      <section>
-        <div>NotFound</div>
+      <section className={styles.notfound}>
+        <Image
+          src='/img/404.png'
+          width={1000}
+          height={500}
+          objectFit='cover'
+          alt='Not Found'
+        />
       </section>
     </>
   )
