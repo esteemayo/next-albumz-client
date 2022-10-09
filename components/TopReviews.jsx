0f23ac014868ca1dafb9da8be1a2b71d573cbe22
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:06:33 +0100

    rendered component dynamically

--- components/TopReviews.jsx
+++ components/TopReviews.jsx
@@ -3,80 +3,40 @@ import Image from 'next/image';
 import StarRating from './StarRating';
 import styles from '@/styles/TopReviews.module.scss';
 
-const TopReviews = () => {
+const TopReviews = ({ reviews }) => {
   return (
     <div className={styles.container}>
       <h3>Testimonials</h3>
       <div className={styles.wrapper}>
-        <div className={styles.reviews}>
-          <div className={styles.reviewer__img}>
-            <Image
-              src='/img/user-1.jpg'
-              width={80}
-              height={80}
-              objectFit='cover'
-              alt=''
-            />
-          </div>
-          <div className={styles.reviewer__review}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              veniam fuga explicabo ex exercitationem autem.
-            </p>
-          </div>
-          <div className={styles.reviewer__rating}>
-            <StarRating value={4} className={styles.ratingIcon} />
-            <span className={styles.ratingValue}>4.0</span>
-          </div>
-          <div className={styles.reviewer__name}>John doe</div>
-        </div>
-        <div className={styles.reviews}>
-          <div className={styles.reviewer__img}>
-            <Image
-              src='/img/user-2.jpeg'
-              width={80}
-              height={80}
-              objectFit='cover'
-              alt=''
-            />
-          </div>
-          <div className={styles.reviewer__review}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              veniam fuga explicabo ex exercitationem autem.
-            </p>
-          </div>
-          <div className={styles.reviewer__rating}>
-            <StarRating value={5} className={styles.ratingIcon} />
-            <span className={styles.ratingValue}>5.0</span>
-          </div>
-          <div className={styles.reviewer__name}>Mary doe</div>
-        </div>
-        <div className={styles.reviews}>
-          <div className={styles.reviewer__img}>
-            <Image
-              src='/img/user-3.jpg'
-              width={80}
-              height={80}
-              objectFit='cover'
-              alt=''
-            />
-          </div>
-          <div className={styles.reviewer__review}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              veniam fuga explicabo ex exercitationem autem.
-            </p>
-          </div>
-          <div className={styles.reviewer__rating}>
-            <StarRating value={3} className={styles.ratingIcon} />
-            <span className={styles.ratingValue}>3.0</span>
-          </div>
-          <div className={styles.reviewer__name}>Chris vega</div>
-        </div>
+        {reviews?.map((item) => {
+          const {_id: id, user, rating, review } = item;
+          return (
+            <div key={id} className={styles.reviews}>
+              <div className={styles.reviewer__img}>
+                <Image
+                  src='/img/user-1.jpg'
+                  width={80}
+                  height={80}
+                  objectFit='cover'
+                  alt=''
+                />
+              </div>
+              <div className={styles.reviewer__review}>
+                <p>{review}</p>
+              </div>
+              <div className={styles.reviewer__rating}>
+                <StarRating value={rating} className={styles.ratingIcon} />
+                <span className={styles.ratingValue}>{rating.toFixed(1)}</span>
+              </div>
+              <div className={styles.reviewer__name}>John doe</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
+
+export const getStaticProps = async () => {};
 
 export default TopReviews;
