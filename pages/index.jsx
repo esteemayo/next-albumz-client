681bcca1864e55baf088ab55c90123c482a7372b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:07:28 +0100

    fetched top reviews and featured albums data respectively

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,15 +8,17 @@ import GenreForm from '@/components/GenreForm';
 import styles from '@/styles/Home.module.scss';
 import TopReviews from '@/components/TopReviews';
 import FeaturedAlbums from '@/components/FeaturedAlbums';
+import { getTopReviews } from '@/services/reviewService';
+import { getFeaturedAlbums } from '@/services/albumService';
 
-const Home = () => {
+const Home = ({ reviews, featuredAlbums, }) => {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
       <Hero />
-      <FeaturedAlbums />
-      <TopReviews />
+      <FeaturedAlbums albums={featuredAlbums} />
+      <TopReviews reviews={reviews} />
       <AddButton text='New album' onClick={() => setShowModal(true)} />
       {showModal && (
         <Modal onClose={setShowModal}>
@@ -25,6 +27,19 @@ const Home = () => {
       )}
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const { data } = await getFeaturedAlbums();
+  const { data : { reviews } } = await getTopReviews();
+
+  return {
+    props: {
+      reviews,
+      featuredAlbums: data.albums,
+    },
+    revalidate: 1,
+  };
 };
 
 export default Home;
