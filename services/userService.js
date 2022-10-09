89a218e89d4a9ce5a811314360dad70055ec5ac3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 20:22:48 +0100

    added next register user endpoint

---
+++ services/userService.js
@@ -0,0 +1,10 @@
+import http from './httpService';
+
+const apiEndpoint = ' /users';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export const register = (credentials) =>
+  http.post(`${apiEndpoint}/register`, credential);
+
+export const nextRegister = (credentials) =>
+  http.post(`${apiUrl}/users/register`, credentials);
