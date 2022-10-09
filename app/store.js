87fb188f61cf481b304d01fe6490c4e768301536
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 20:22:11 +0100

    imported authSlice

--- app/store.js
+++ app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import authReducer from '@/features/auth/authSlice';
 import toggleReducer from '@/features/toggle/toggleSlice';
 import darkModeReducer from '@/features/darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     toggle: toggleReducer,
     darkMode: darkModeReducer,
   },
