ad8bc82943c57bfd86ce3eb5af1cbf1ff16dda79
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:04:35 +0100

    added env variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   images: {
     domains: ['source.unsplash.com', 'i.pravatar.cc']
+  },
+  env: {
+    NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
+    NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   }
 }
 
