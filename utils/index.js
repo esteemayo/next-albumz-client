95709cde3a23d687c7e773dafcbb5a1671447c34
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:15:41 +0100

    first commit

---
+++ utils/index.js
@@ -0,0 +1,30 @@
+import cookie from 'cookie';
+
+export const excerpts = (str, count) => {
+  if (str.length > count) {
+    str = str.substring(0, count) + ' ...';
+  }
+  return str;
+};
+
+export const getFromStorage = (key) => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem(key);
+  }
+};
+
+export const setToStorage = (key, value) => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export const removeFromStorage = (key) => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.removeItem(key);
+  }
+};
+
+export const parseCookie = (req) =>
+  cookie.parse(req ? req.headers.cookie || '' : '');
+  
