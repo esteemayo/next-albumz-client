b5676784d60589f176407d4597d363e308dbe6a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:13:42 +0100

    added album endpoints

---
+++ services/albumService.js
@@ -0,0 +1,29 @@
+import http from './httpService';
+
+const apiEndpoint = '/albums';
+
+const albumUrl = (albumId) => `${apiEndpoint}/${albumId}`;
+
+export const getAlbums = () => http.get(apiEndpoint);
+
+export const getFeaturedAlbums = () => http.get(`${apiEndpoint}/featured`);
+
+export const getUserAlbums = () => http.get(`${apiEndpoint}/user-albums`);
+
+export const getTopAlbums = () => http.get(`${apiEndpoint}/top`);
+
+export const getAlbumsByTag = (tag) => http.get(`${apiEndpoint}/tags/${tag}`);
+
+export const getAlbumById = (albumId) => http.get(albumUrl(albumId));
+
+export const getAlbumBySlug = (slug) => http.get(`${apiEndpoint}/details/${slug}`);
+
+export const createAlbum = (album) => http.post(apiEndpoint, album);
+
+export const createReview = (albumId, review) => http.post(`${apiEndpoint}/${albumId}/reviews`);
+
+export const updateAlbum = (albumId, album) => http.patch(albumUrl(albumId), album);
+
+export const likeAlbum = (albumId) => http.patch(`${apiEndpoint}/like-album/${albumId}`);
+
+export const deleteAlbum = (albumId) => http.delete(albumUrl(albumId));
