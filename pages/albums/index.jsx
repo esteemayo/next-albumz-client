1a978427d40bdfb20eec806fef195a84834cfc2a
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:05:00 +0100

    fetched albums data

--- pages/albums/index.jsx
+++ pages/albums/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import Modal from '@/components/Modal';
 import AddButton from '@/components/AddButton';
@@ -6,19 +7,17 @@ import AlbumCard from '@/components/AlbumCard';
 import AlbumForm from '@/components/AlbumForm';
 import Pagination from '@/components/Pagination';
 import styles from '@/styles/Albums.module.scss';
+import { getAlbums } from '@/services/albumService';
 
-const Albums = () => {
+const Albums = ({ albums }) => {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
+        {albums?.map((item => {
+          return <AlbumCard key={item._id} {...item} />;
+        }))}
       </div>
       <Pagination />
       
@@ -30,6 +29,31 @@ const Albums = () => {
       )}
     </main>
   );
+};
+
+Albums.propTypes = {
+  albums: PropTypes.arrayOf(
+    PropTypes.shape({
+      tags: PropTypes.array.isRequired,
+      info: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      ratingsAverage: PropTypes.number.isRequired,
+      ratingsQuantity: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+export const getStaticProps = async () => {
+  const { data } = await getAlbums();
+
+  return {
+    props: {
+      albums: data.albums,
+    },
+    revalidate: 1,
+  };
 };
 
 export default Albums;
