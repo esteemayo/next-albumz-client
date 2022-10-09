42877730ca98b411017b15ce54c4f5a661e789f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 20:21:46 +0100

    added tokenKey

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,7 @@
 import cookie from 'cookie';
 
+export const tokenKey = 'token';
+
 export const excerpts = (str, count) => {
   if (str.length > count) {
     str = str.substring(0, count) + ' ...';
