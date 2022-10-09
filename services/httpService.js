c5519a419f621c218fc0d225e2e4924429c58409
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:13:58 +0100

    modified

---
+++ services/httpService.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+import logger from './logService';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const NEXT_PUBLIC_DEV_API_URL = process.env.NEXT_PUBLIC_DEV_API_URL;
+const NEXT_PUBLIC_PROD_API_URL = process.env.NEXT_PUBLIC_PROD_API_URL;
+
+const authFetch = axios.create({
+  baseURL: `${devEnv ? NEXT_PUBLIC_DEV_API_URL : NEXT_PUBLIC_PROD_API_URL}`,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+authFetch.interceptors.request.use(
+  (req) => {
+    req.headers.common['Authorization'] = `Bearer`;
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if(!expectedError) {
+    logger.log(error);
+    toast.error('An unexpected error occurred');
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: authFetch.get,
+  post: authFetch.post,
+  patch: authFetch.patch,
+  delete: authFetch.delete,
+};
+
+export default http;
