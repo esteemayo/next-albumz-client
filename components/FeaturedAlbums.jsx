e3339bae58888de838bc35ade27fc2b98de7fe45
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-09 19:03:05 +0100

    rendered component dynamically

--- components/FeaturedAlbums.jsx
+++ components/FeaturedAlbums.jsx
@@ -1,21 +1,36 @@
 import Image from 'next/image';
+import PropTypes from 'prop-types';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 import AlbumCard from '@/components/AlbumCard';
 import styles from '@/styles/FeaturedAlbums.module.scss';
 
-const FeaturedAlbums = () => {
+const FeaturedAlbums = ({ albums }) => {
   return (
     <div className={styles.container}>
       <h2 className={styles.heading__secondary}>Featured albums</h2>
       <div className={styles.wrapper}>
-        <AlbumCard />
-        <AlbumCard />
-        <AlbumCard />
+        {albums?.map((item) => {
+          return <AlbumCard key={item._id} {...item} />;
+        })}
       </div>
     </div>
   );
+};
+
+FeaturedAlbums.propTypes = {
+  albums: PropTypes.arrayOf(
+    PropTypes.shape({
+      tags: PropTypes.array.isRequired,
+      info: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      ratingsAverage: PropTypes.number.isRequired,
+      ratingsQuantity: PropTypes.number.isRequired,
+    }),
+  ),
 };
 
 export default FeaturedAlbums;
