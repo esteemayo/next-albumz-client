6f317e60df1e3761a14f9e1b442ef1ea4b018512
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-11 00:39:32 +0100

    configured cloudinary under images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['source.unsplash.com', 'i.pravatar.cc']
+    domains: ['res.cloudinary.com', 'source.unsplash.com', 'i.pravatar.cc']
   },
   env: {
     NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
     NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   }
 }
 
