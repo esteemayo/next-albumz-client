028526ea13699b45c4f4a7c7ab8ade84efdcd91b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-10 02:26:07 +0100

    fetched logged in user data

--- pages/users/dashboard.jsx
+++ pages/users/dashboard.jsx
@@ -1,19 +1,33 @@
 import Meta from '@/components/Meta';
+import { parseCookie } from '@/utils/index';
 import styles from '@/styles/Dashboard.module.scss';
 import DashboardCard from '@/components/DashboardCard';
+import { getUserAlbums } from '@/services/albumService';
 
-const Dashboard = () => {
+const Dashboard = ({ albums }) => {
+  console.log(albums)
   return (
     <>
       <Meta title='User Dashboard - Albumz Music Entertainment' />
       <section className={styles.container}>
         <h1 className={styles.header}>Dashboard</h1>
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
+        {albums.map((item) => {
+          return <DashboardCard key={item._id} {...item} />;
+        })}
       </section>
     </>
   );
+};
+
+export const getServerSideProps = async ({ req }) => {
+  const { token } = parseCookie(req);
+  const { data } = await getUserAlbums(token);
+
+  return {
+    props: {
+      albums: data.albums,
+    },
+  };
 };
 
 export default Dashboard;
