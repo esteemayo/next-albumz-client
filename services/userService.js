8a95cdcb39d40bd358186392148d4f4e557fd644
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-11 16:54:04 +0100

    fixed a bug

--- services/userService.js
+++ services/userService.js
@@ -4,7 +4,7 @@ const apiEndpoint = ' /users';
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const register = (credentials) =>
-  http.post(`${apiEndpoint}/register`, credential);
+  http.post(`${apiEndpoint}/register`, credentials);
 
 export const nextRegister = (credentials) =>
   http.post(`${apiUrl}/users/register`, credentials);
