146845b3ae1f39aca70503474fbaf39e147409ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-12 20:56:32 +0100

    used lazy loading

--- components/FeaturedAlbums.jsx
+++ components/FeaturedAlbums.jsx
@@ -1,19 +1,22 @@
 import Image from 'next/image';
 import PropTypes from 'prop-types';
+import { lazy, Suspense } from 'react';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
-import AlbumCard from '@/components/AlbumCard';
 import styles from '@/styles/FeaturedAlbums.module.scss';
+const AlbumCard = lazy(() => import('@/components/AlbumCard'));
 
 const FeaturedAlbums = ({ albums }) => {
   return (
     <div className={styles.container}>
       <h2 className={styles.heading__secondary}>Featured albums</h2>
       <div className={styles.wrapper}>
-        {albums?.map((item) => {
-          return <AlbumCard key={item._id} {...item} />;
-        })}
+        <Suspense fallback={<h1>Loading</h1>}>
+          {albums?.map((item) => {
+            return <AlbumCard key={item._id} {...item} />;
+          })}
+        </Suspense>
       </div>
     </div>
   );
