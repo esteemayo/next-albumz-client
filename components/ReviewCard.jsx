45532b68bba1f27e1d771f4de3915c3fe9b39e7f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-12 02:05:50 +0100

    rendered reviews dynamically

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -3,19 +3,16 @@ import Image from 'next/image';
 import StarRating from '@/components/StarRating';
 import styles from '@/styles/ReviewCard.module.scss';
 
-const ReviewCard = () => {
+const ReviewCard = ({ rating, review, user }) => {
   return (
     <div className={styles.review}>
       <div className={styles.review__card}>
         <div className={styles.left}>
           <div className={styles.reviewer}>
-            <h5 className={styles.reviewer__name}>Aegen magazines</h5>
-            <StarRating value={3.5} className={styles.reviewer__rating} />
+            <h5 className={styles.reviewer__name}>{user.name}</h5>
+            <StarRating value={rating} className={styles.reviewer__rating} />
           </div>
-          <p className={styles.reviewer__review}>
-            Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing 
-            industries for previewing layouts and visual mockups. credit (www.brighttv.co.th)
-          </p>
+          <p className={styles.reviewer__review}>{review}</p>
           <div className={styles.line}>&nbsp;</div>
           <div className={styles.date}>
             <span>8:35 PM - Jan 4, 2022</span>
@@ -24,7 +21,7 @@ const ReviewCard = () => {
         <div className={styles.right}>
           <div className={styles.image__wrapper}>
             <Image
-              src='/img/user-1.jpg'
+              src={user.avatar ? user.avatar : '/img/user-default.jpg'}
               width={80}
               height={80}
               objectFit='cover'
