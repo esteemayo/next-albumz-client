32f52949676ca2012ff4d9fe8b5cc54ec961a3b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-13 00:29:20 +0100

    rendered component dynamically

--- components/RelatedAlbumCard.jsx
+++ components/RelatedAlbumCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 import styles from '@/styles/RelatedAlbumCard.module.scss';
 
-const RelatedAlbumCard = () => {
+const RelatedAlbumCard = ({ image, title, artist, createdAt }) => {
   return (
     <div className={styles.related__card}>
       <div className={styles.left}>
@@ -12,18 +12,18 @@ const RelatedAlbumCard = () => {
           width='70'
           height='70'
           objectFit='cover'
-          alt=''
+          alt={title}
         />
       </div>
       <div className={styles.right}>
         <h6 className={styles.title}>
           <Link href={`albums/slug`} passHref>
-            <a className={styles.link}>A better time</a>
+            <a className={styles.link}>{title}</a>
           </Link>
         </h6>
-        <p className={styles.artist}>Davido</p>
+        <p className={styles.artist}>{artist}</p>
         <span className={styles.year}>
-          {new Date().toLocaleString('en-us', {
+          {new Date(createdAt).toLocaleString('en-us', {
             day: 'numeric',
             month: 'long',
             year: 'numeric',
