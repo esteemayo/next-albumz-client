d4f793b97a1958d3074b27dbc9b43ea8c2a3620e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-12 02:18:34 +0100

    formatted the date

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import StarRating from '@/components/StarRating';
 import styles from '@/styles/ReviewCard.module.scss';
 
-const ReviewCard = ({ rating, review, user }) => {
+const ReviewCard = ({ rating, review, user, createdAt }) => {
   return (
     <div className={styles.review}>
       <div className={styles.review__card}>
@@ -15,7 +15,7 @@ const ReviewCard = ({ rating, review, user }) => {
           <p className={styles.reviewer__review}>{review}</p>
           <div className={styles.line}>&nbsp;</div>
           <div className={styles.date}>
-            <span>8:35 PM - Jan 4, 2022</span>
+            <span>8:35 PM - {new Date(createdAt).toDateString()}</span>
           </div>
         </div>
         <div className={styles.right}>
