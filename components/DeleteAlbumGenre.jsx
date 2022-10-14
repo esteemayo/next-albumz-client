85658777d419c038292ab36a083d2b2edd868741
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-14 15:17:23 +0100

    handled delete album and genre api calls

--- components/DeleteAlbumGenre.jsx
+++ components/DeleteAlbumGenre.jsx
@@ -1,6 +1,26 @@
+import { toast } from 'react-toastify';
+
+import { deleteAlbum } from '@/services/albumService';
+import { deleteGenre } from '@/services/genreService';
 import styles from '@/styles/DeleteAlbumGenre.module.scss';
 
-const DeleteGenreAlbum = ({ title, closeModal }) => {
+const DeleteGenreAlbum = ({ type, title, albumId, genreId, closeModal, setGenreList, setAlbumList }) => {
+  const handleDelete = async () => {
+    try {
+      if (type === 'genre') {
+        await deleteGenre(genreId);
+        setGenreList((prev) => prev.filter((item) => item._id !== genreId));
+        return toast.success('Genre deleted successfully');
+      } else if (type === 'album') {
+        await deleteAlbum(albumId);
+        setAlbumList((prev) => prev.filter((item) => item._id !== albumId));
+        return toast.success('Album deleted successfully');
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  };
+  
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -14,7 +34,7 @@ const DeleteGenreAlbum = ({ title, closeModal }) => {
           </button>
           <button
             onClick={()=> {
-              console.log('album/genre deleted');
+              handleDelete();
               closeModal(true);
             }}
             className={`${styles.btn} ${styles.btnDelete}`}
