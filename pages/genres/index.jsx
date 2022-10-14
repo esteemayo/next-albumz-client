ee4626ec897e6be5e4245e74d2cee26175ec5797
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-14 01:13:32 +0100

    fetched user genres

--- pages/genres/index.jsx
+++ pages/genres/index.jsx
@@ -1,16 +1,19 @@
 import Link from 'next/link';
 import { useState } from 'react';
+import Moment from 'react-moment';
 import TagOutlinedIcon from '@mui/icons-material/TagOutlined';
 
 import Meta from '@/components/Meta';
 import Modal from '@/components/Modal';
+import { parseCookie } from '@/utils/index';
 import AddButton from '@/components/AddButton';
 import DialogBox from '@/components/DialogBox';
 import GenreForm from '@/components/GenreForm';
 import styles from '@/styles/Genres.module.scss';
+import { getGenres } from '@/services/genreService';
 import DeleteAlbumGenre from '@/components/DeleteAlbumGenre';
 
-const Genres = () => {
+const Genres = ({ genres }) => {
   const [open, setOpen] = useState(false);
   const [showModal, setShowModal] = useState(true);
   
@@ -31,87 +34,38 @@ const Genres = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className={styles.tr}>
-              <td>
-                <span className={styles.order}>1</span>
-              </td>
-              <td>
-                <span className={styles.name}>Afro pop</span>
-              </td>
-              <td>
-                <span className={styles.postedBy}>John doe</span>
-              </td>
-              <td>
-                <span className={styles.postedOn}>
-                  {new Date().toLocaleString('en-us', { day: 'numeric', month: 'long', year: 'numeric'})}
-                </span>
-              </td>
-              <td className={styles.buttonContainer}>
-                <Link href={`/genres/edit/slug`} passHref>
-                  <a className={styles.btnUpdate}>Update</a>
-                </Link>
-                <button
-                  className={styles.btnDelete}
-                  onClick={() => setShowModal(false)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr className={styles.tr}>
-              <td>
-                <span className={styles.order}>2</span>
-              </td>
-              <td>
-                <span className={styles.name}>Afro pop</span>
-                </td>
-              <td>
-                <span className={styles.postedBy}>John doe</span>
-              </td>
-              <td>
-                <span className={styles.postedOn}>
-                  {new Date().toLocaleString('en-us', { day: 'numeric', month: 'long', year: 'numeric'})}
-                </span>
-              </td>
-              <td className={styles.buttonContainer}>
-                <Link href={`/genres/edit/slug`} passHref>
-                  <a className={styles.btnUpdate}>Update</a>
-                </Link>
-                <button
-                  className={styles.btnDelete}
-                  onClick={() => setShowModal(false)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr className={styles.tr}>
-              <td>
-                <span className={styles.order}>3</span>
-              </td>
-              <td>
-                <span className={styles.name}>Afro pop</span>
-                </td>
-              <td>
-                <span className={styles.postedBy}>John doe</span>
-              </td>
-              <td>
-                <span className={styles.postedOn}>
-                  {new Date().toLocaleString('en-us', { day: 'numeric', month: 'long', year: 'numeric'})}
-                </span>
-              </td>
-              <td className={styles.buttonContainer}>
-                <Link href={`/genres/edit/slug`} passHref>
-                  <a className={styles.btnUpdate}>Update</a>
-                </Link>
-                <button 
-                  className={styles.btnDelete}
-                  onClick={() => setShowModal(false)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
+            {genres?.map((item, index) => {
+              const { _id: id, name, user, slug, createdAt } = item;
+              return (
+                <tr key={id} className={styles.tr}>
+                  <td>
+                    <span className={styles.order}>{index + 1}</span>
+                  </td>
+                  <td>
+                    <span className={styles.name}>{name}</span>
+                  </td>
+                  <td>
+                    <span className={styles.postedBy}>{user?.name}</span>
+                  </td>
+                  <td>
+                    <span className={styles.postedOn}>
+                      <Moment fromNow>{createdAt}</Moment>
+                    </span>
+                  </td>
+                  <td className={styles.buttonContainer}>
+                    <Link href={`/genres/edit/${slug}`} passHref>
+                      <a className={styles.btnUpdate}>Update</a>
+                    </Link>
+                    <button
+                      className={styles.btnDelete}
+                      onClick={() => setShowModal(false)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </section>
@@ -132,6 +86,27 @@ const Genres = () => {
       )}
     </>
   );
+};
+
+export const getServerSideProps = async ({ req }) => {
+  const { token } = parseCookie(req);
+
+  if (!token || token === '') {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const { data } = await getGenres(token);
+
+  return {
+    props: {
+      genres: data.genres,
+    },
+  };
 };
 
 export default Genres;
