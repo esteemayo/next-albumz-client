8bd10069ec77c0fa0afb03fe55ad425c75e934ed
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-16 20:54:11 +0100

    handled search operation

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import SearchIcon from '@mui/icons-material/Search';
 
 import styles from '@/styles/Search.module.scss';
 
 const Search = () => {
+  const router = useRouter();
   const [query, setQuery] = useState('');
 
   const handleSearch = (e) => {
     e.preventDefault();
+
+    if (query) {
+      router.push(`/albums/search?q=${query}`);
+      setQuery('');
+    }
   };
 
   return (
     <form onSubmit={handleSearch} className={styles.search}>
       <input
         type='search'
+        value={query}
         placeholder='Search Albums'
         className={styles.search__input}
         onChange={(e) => setQuery(e.target.value)}
