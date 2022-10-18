e6aaaea80b4641cfba8655b1b6b757de031c7d37
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-18 15:38:42 +0100

    handled views api calls

--- features/views/viewSlice.js
+++ features/views/viewSlice.js
@@ -1,6 +1,30 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { createView, getViewsOnAlbum } from '@/services/viewService';
 
+const getViews =  createAsyncThunk(
+  'views/getViews',
+  async (albumId, { rejectWithValue }) => {
+    try {
+      const { data } = await getViewsOnAlbum({ album: albumId });
+      return data.histories;
+    } catch (err) {
+      rejectWithValue(err.response.data);
+    }
+  }
+);
+
+const createViews =  createAsyncThunk(
+  'views/createViews',
+  async (album, { rejectWithValue }) => {
+    try {
+      const { data } = await createView(album);
+      return data.history;
+    } catch (err) {
+      rejectWithValue(err.response.data);
+    }
+  }
+);
+
 const initialState = {
   views: [],
   isError: false,
@@ -14,6 +38,37 @@ export const viewSlice = createSlice({
   initialState,
   reducers: {
     reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getViews.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getViews.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.views = payload;
+      })
+      .addCase(getViews.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = payload.message;
+      })
+      .addCase(createViews.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createViews.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.views.push(payload);
+      })
+      .addCase(createViews.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = payload.message;
+      });
   },
 });
 
