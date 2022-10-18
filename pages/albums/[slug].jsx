77b749f971f04051f760ec5d560edc2fcc25c8db
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-18 16:00:39 +0100

    modified

--- pages/albums/[slug].jsx
+++ pages/albums/[slug].jsx
@@ -1,6 +1,5 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
 import Meta from '@/components/Meta';
 import Reviews from '@/components/Reviews';
@@ -11,53 +10,22 @@ import styles from '@/styles/SingleAlbum.module.scss';
 import RelatedAlbums from '@/components/RelatedAlbums';
 import SingleAlbumHero from '@/components/SingleAlbumHero';
 import AlbumDescription from '@/components/AlbumDescription';
-import { getViews } from '@/features/views/viewSlice';
 
 const SingleAlbum = ({ album }) => {
-  const dispatch = useDispatch();
-  const { views } = useSelector((state) => ({ ...state.views }));
-
   const [reviews, setReviews] = useState(album.reviews);
-
-  const albumId = album?._id;
-  
-  useEffect(() => {
-    dispatch(getViews(albumId));
-  }, [albumId, dispatch]);
 
   return (
     <>
       <Meta title={`${album.title} - Albumz Music Entertainment`} />
       <section className={styles.container}>
         <SingleAlbumHero album={album} />
-        <AlbumDescription album={album} views={views} />
+        <AlbumDescription album={album} />
         <ReviewForm albumId={album.id} setReviews={setReviews} />
         <Reviews reviews={reviews} />
         <RelatedAlbums albumId={album.id} tags={album.tags} />
       </section>
     </>
   );
-};
-
-export const getServerSideProps = async ({ req, params: { slug } }) => {
-  const { token } = parseCookie(req);
-
-  if (!token || token === '') {
-    return {
-      redirect: {
-        destination: '/auth/login',
-        permanent: false,
-      }
-    };
-  }
-
-  const { data } = await albumAPI.getAlbumBySlug(slug, token);
-
-  return {
-    props: {
-      album: data.album,
-    },
-  };
 };
 
 SingleAlbum.propTypes = {
@@ -79,6 +47,27 @@ SingleAlbum.propTypes = {
     image: PropTypes.string.isRequired,
     createdAt: PropTypes.string.isRequired,
   }),
+};
+
+export const getServerSideProps = async ({ req, params: { slug } }) => {
+  const { token } = parseCookie(req);
+
+  if (!token || token === '') {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      }
+    };
+  }
+
+  const { data } = await albumAPI.getAlbumBySlug(slug, token);
+
+  return {
+    props: {
+      album: data.album,
+    },
+  };
 };
 
 export default SingleAlbum;
