eada16c9b75fae662b648fa9a081655f418714ec
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-18 03:56:12 +0100

    added bookmark slice

--- app/store.js
+++ app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '@/features/auth/authSlice';
 import toggleReducer from '@/features/toggle/toggleSlice';
+import bookmarkReducer from '@/features/bookmark/bookmarkSlice';
 import darkModeReducer from '@/features/darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     toggle: toggleReducer,
+    bookmark: bookmarkReducer,
     darkMode: darkModeReducer,
   },
 });
