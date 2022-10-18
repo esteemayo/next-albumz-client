8cc887832d01b263679e095f53e5c4e9960f4f92
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-18 05:35:28 +0100

    added views reducer to the store

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '@/features/auth/authSlice';
+import viewsReducer from '@/features/views/viewSlice';
 import toggleReducer from '@/features/toggle/toggleSlice';
 import bookmarkReducer from '@/features/bookmark/bookmarkSlice';
 import darkModeReducer from '@/features/darkMode/darkModeSlice';
@@ -8,6 +9,7 @@ import darkModeReducer from '@/features/darkMode/darkModeSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    views: viewsReducer,
     toggle: toggleReducer,
     bookmark: bookmarkReducer,
     darkMode: darkModeReducer,
