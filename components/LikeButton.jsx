72537c0a3e06ab11289134336341fea3c7e5fb06
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-19 15:17:15 +0100

    performed like functionalities

--- components/LikeButton.jsx
+++ components/LikeButton.jsx
@@ -1,15 +1,46 @@
+import { useSelector } from 'react-redux';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 import { likeAlbum } from '@/services/albumService';
 import styles from '@/styles/LikeButton.module.scss';
 
-const LikeButton = ({ type }) => {
-  return (
-    <div className={styles.icon__wrapper}>
-      <FavoriteBorderOutlinedIcon 
+const LikeButton = ({ type, likes }) => {
+  const { user } = useSelector((state) => ({ ...state.auth }));
+
+  const Likes = () => {
+    if (likes.length > 0) {
+      return likes.find(like => like === user._id) ? (
+        <Tooltip TransitionComponent={Zoom} title={`You and ${likes.length - 1} other peoples like`} arrow>
+          <IconButton>
+            <FavoriteOutlinedIcon
+              className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
+            />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip TransitionComponent={Zoom} title={`${likes.length} Like${likes.length > 1 ? 's' : ''}`} arrow>
+          <IconButton>
+            <FavoriteBorderOutlinedIcon
+              className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
+            />
+          </IconButton>
+        </Tooltip>
+      );
+    }
+
+    return (
+      <FavoriteBorderOutlinedIcon
         className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
       />
+    );
+  };
+
+  return (
+    <div className={styles.icon__wrapper}>
+      {Likes()}
     </div>
   );
 };
