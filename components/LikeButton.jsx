c7a9a314c02c88c5b467211ea9302ed9099240cf
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-19 19:26:47 +0100

    imported Zoom from material ui

--- components/LikeButton.jsx
+++ components/LikeButton.jsx
@@ -1,3 +1,4 @@
+import Zoom from '@mui/material/Zoom';
 import { useSelector } from 'react-redux';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
@@ -7,15 +8,24 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 import { likeAlbum } from '@/services/albumService';
 import styles from '@/styles/LikeButton.module.scss';
 
-const LikeButton = ({ type, likes }) => {
+const LikeButton = ({ type, likes, albumId }) => {
   const { user } = useSelector((state) => ({ ...state.auth }));
+
+  const handleLike = async () => {
+    try {
+      await likeAlbum(albumId);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const Likes = () => {
     if (likes.length > 0) {
-      return likes.find(like => like === user._id) ? (
+      return likes.find(like => like === user?._id) ? (
         <Tooltip TransitionComponent={Zoom} title={`You and ${likes.length - 1} other peoples like`} arrow>
           <IconButton>
             <FavoriteOutlinedIcon
+              onClick={!user ? null : handleLike}
               className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
             />
           </IconButton>
@@ -24,6 +34,7 @@ const LikeButton = ({ type, likes }) => {
         <Tooltip TransitionComponent={Zoom} title={`${likes.length} Like${likes.length > 1 ? 's' : ''}`} arrow>
           <IconButton>
             <FavoriteBorderOutlinedIcon
+              onClick={!user ? null : handleLike}
               className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
             />
           </IconButton>
@@ -33,6 +44,7 @@ const LikeButton = ({ type, likes }) => {
 
     return (
       <FavoriteBorderOutlinedIcon
+        onClick={!user ? null : handleLike}
         className={type === 'single' ? `${styles.like__icon} ${styles.action__icon}` : `${styles.like__icon}`}
       />
     );
