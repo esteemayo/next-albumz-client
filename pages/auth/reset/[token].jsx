cc6a5251e2644fc22558333bb4cb52ccaeb697de
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-30 07:55:14 +0100

    modified

--- pages/auth/reset/[token].jsx
+++ pages/auth/reset/[token].jsx
@@ -116,7 +116,7 @@ const ResetPassword = () => {
             </form>
           </div>
           <div className={styles.links}>
-            <Link href='/auth/login' passHref>Return to Lost Password</Link>
+            <Link href='/auth/forgot' passHref>Return to Lost Password</Link>
           </div>
         </div>
       </div>
