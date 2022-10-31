f1e48e61f49cfdb4b7db7009e66a5e65ab4c8a62
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-10-31 20:59:37 +0100

    changed reviews to a separate component

--- components/TopReviews.jsx
+++ components/TopReviews.jsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
-
-import StarRating from './StarRating';
+import TopReview from './TopReview';
 import styles from '@/styles/TopReviews.module.scss';
 
 const TopReviews = ({ reviews }) => {
@@ -9,28 +7,7 @@ const TopReviews = ({ reviews }) => {
       <h3>Testimonials</h3>
       <div className={styles.wrapper}>
         {reviews?.map((item) => {
-          const {_id: id, user, rating, review } = item;
-          return (
-            <div key={id} className={styles.reviews}>
-              <div className={styles.reviewer__img}>
-                <Image
-                  src='/img/user-1.jpg'
-                  width={80}
-                  height={80}
-                  objectFit='cover'
-                  alt=''
-                />
-              </div>
-              <div className={styles.reviewer__review}>
-                <p>{review}</p>
-              </div>
-              <div className={styles.reviewer__rating}>
-                <StarRating value={rating} className={styles.ratingIcon} />
-                <span className={styles.ratingValue}>{rating.toFixed(1)}</span>
-              </div>
-              <div className={styles.reviewer__name}>John doe</div>
-            </div>
-          );
+          return <TopReview key={item._id} {...item} />;
         })}
       </div>
     </div>
