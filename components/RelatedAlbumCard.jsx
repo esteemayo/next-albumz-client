afa648775ab0016bcfca33f37967d7c19a39d212
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-01 21:14:43 +0100

    modified

--- components/RelatedAlbumCard.jsx
+++ components/RelatedAlbumCard.jsx
@@ -9,7 +9,7 @@ const RelatedAlbumCard = ({ slug, image, title, artist, createdAt }) => {
     <div className={styles.related__card}>
       <div className={styles.left}>
         <Image
-          src='/img/banner.jpg'
+          src={image ?? '/img/banner.jpg'}
           width='70'
           height='70'
           objectFit='cover'
