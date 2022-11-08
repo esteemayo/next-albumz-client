39686143f8f5869595a68d6ec47bcd20a61adec1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-08 17:56:54 +0100

    sorted reviews array

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -2,13 +2,17 @@ import ReviewCard from './ReviewCard';
 import styles from '@/styles/Reviews.module.scss';
 
 const Reviews = ({ reviews }) => {
+  const sorted = reviews.sort((a, b) => a.createdAt - b.createdAt)
+  console.log(sorted)
   return (
     <>
       {reviews.length > 0 && (
         <section className={styles.reviews}>
           <h4 className={styles.header}>{reviews.length > 1 ? 'Reviews' : 'Review'}</h4>
-          {reviews.map((item) => {
-            return <ReviewCard key={item._id} {...item} />;
+          {reviews
+            .sort((a,b) => b.createdAt - a.createdAt)
+            .map((item) => {
+              return <ReviewCard key={item._id} {...item} />;
           })}
         </section>
       )}
