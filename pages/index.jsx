6dde4b8ac670cff26214f5c3ca6f172bced0c919
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-14 00:00:26 +0100

    fixed hydration error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
 import { useSelector } from 'react-redux';
 
 import Hero from '@/components/Hero';
 import Modal from '@/components/Modal';
 import AddButton from '@/components/AddButton';
 import AlbumForm from '@/components/AlbumForm';
-import TopReviews from '@/components/TopReviews';
 import { getAllGenres } from '@/services/genreService';
-import FeaturedAlbums from '@/components/FeaturedAlbums';
 import { getTopReviews } from '@/services/reviewService';
 import { getFeaturedAlbums } from '@/services/albumService';
+
+const TopReviews = dynamic(() => import('@/components/TopReviews'), { ssr: false });
+const FeaturedAlbums = dynamic(() => import('@/components/FeaturedAlbums'), { ssr: false });
 
 const Home = ({ genres, reviews, featuredAlbums }) => {
   const [showModal, setShowModal] = useState(false);
