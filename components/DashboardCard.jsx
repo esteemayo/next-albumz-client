42947dcdd0e200792d78c39fa87713d51e45c03f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-14 02:22:24 +0100

    modified

--- components/DashboardCard.jsx
+++ components/DashboardCard.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { lazy, Suspense, useState } from 'react';
 
 import { excerpts } from '@/utils/index';
+import Spinner from '@/components/Spinner';
 import DialogBox from '@/components/DialogBox';
 import styles from '@/styles/DashboardCard.module.scss';
 import DeleteAlbumGenre from '@/components/DeleteAlbumGenre';
@@ -15,47 +16,49 @@ const DashboardCard = ({ albums }) => {
 
   return (
     <>
-      {albumList?.map((item) => {
-        const { _id: id, info, slug, image, title } = item;
-        return (
-          <div key={id} className={styles.wrapper}>
-            <div className={styles.card}>
-              <div className={styles.card__left}>
-                <div className={styles.card__img}>
-                  <div className={styles.overlay}>&nbsp;</div>
-                  <Image
-                    src={image ? image : '/img/default-album-2.jpeg'}
-                    width={200}
-                    height={130}
-                    alt={title}
-                  />
+      <Suspense fallback={<Spinner />}>
+        {albumList?.map((item) => {
+          const { _id: id, info, slug, image, title } = item;
+          return (
+            <div key={id} className={styles.wrapper}>
+              <div className={styles.card}>
+                <div className={styles.card__left}>
+                  <div className={styles.card__img}>
+                    <div className={styles.overlay}>&nbsp;</div>
+                    <Image
+                      src={image ? image : '/img/default-album-2.jpeg'}
+                      width={200}
+                      height={130}
+                      alt={title}
+                    />
+                  </div>
+                  <h3 className={styles.card__heading}>
+                    <span>{title}</span>
+                  </h3>
                 </div>
-                <h3 className={styles.card__heading}>
-                  <span>{title}</span>
-                </h3>
-              </div>
-              <div className={styles.card__right}>
-                <p className={styles.card__info}>{info && excerpts(info, 200)}</p>
-                <div className={styles.card__line}>&nbsp;</div>
-                <div className={styles.card__button}>
-                  <Link href={`/albums/edit/${slug}`} passHref>
-                    <a className={styles.card__updateBtn}>Update</a>
-                  </Link>
-                  <button
-                    onClick={() => {
-                      setShowModal(false);
-                      setAlbumId(id);
-                    }}
-                    className={styles.card__delete}
-                  >
-                    Delete
-                  </button>
+                <div className={styles.card__right}>
+                  <p className={styles.card__info}>{info && excerpts(info, 200)}</p>
+                  <div className={styles.card__line}>&nbsp;</div>
+                  <div className={styles.card__button}>
+                    <Link href={`/albums/edit/${slug}`} passHref>
+                      <a className={styles.card__updateBtn}>Update</a>
+                    </Link>
+                    <button
+                      onClick={() => {
+                        setShowModal(false);
+                        setAlbumId(id);
+                      }}
+                      className={styles.card__delete}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </Suspense>
       {!showModal && (
         <DialogBox>
           <DeleteAlbumGenre
