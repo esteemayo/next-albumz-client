c81d374e7ca7e0a5e5a61d98c87c3dcb46efb261
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-16 14:14:57 +0100

    fix: modify

--- pages/albums/top.jsx
+++ pages/albums/top.jsx
@@ -44,7 +44,7 @@ const TopAlbums = ({ albums }) => {
                       width={100}
                       height={100}
                       objectFit='cover'
-                      alt={title}
+                      alt=''
                     />
                   </td>
                   <td>
