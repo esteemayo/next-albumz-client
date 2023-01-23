d27fc65f65d042d09dc3ab81b5fbc69665406a70
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-23 22:35:25 +0100

    feat: add middleware

--- app/store.js
+++ app/store.js
@@ -36,6 +36,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export default store;
