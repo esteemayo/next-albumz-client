be75e62958dc905cd84bda95b192dd35760a0ebb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-23 22:38:33 +0100

    feat: import store/persistor

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '@/app/store';
+import { persistor, store} from '@/app/store';
 import Layout from '@/components/Layout';
 import ScrollToTop from '@/components/ScrollToTop';
 
