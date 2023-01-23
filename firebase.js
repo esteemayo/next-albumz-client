06cfa3a2768c58234fbe3b76064a450ab33f96a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-23 08:04:16 +0100

    feat: add firebase config file

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'albums-cc519.firebaseapp.com',
+  projectId: 'albums-cc519',
+  storageBucket: 'albums-cc519.appspot.com',
+  messagingSenderId: '218675999859',
+  appId: '1:218675999859:web:bbdd327c0a8298ec462606'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
