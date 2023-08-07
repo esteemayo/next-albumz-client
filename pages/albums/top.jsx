7129f4d5fecf2d3e884bde69e3efd9775bc6ed0d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 17:13:54 +0100

    fix: modify

--- pages/albums/top.jsx
+++ pages/albums/top.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { useState } from 'react';
+import Image from 'next/image';
 import TagOutlinedIcon from '@mui/icons-material/TagOutlined';
 
 import Meta from '@/components/Meta';
