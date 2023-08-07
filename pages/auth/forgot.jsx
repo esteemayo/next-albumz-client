877139b560245bfe7bc9d7210327e220aa196544
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 18:31:21 +0100

    fix: add useCallback, react hook

--- pages/auth/forgot.jsx
+++ pages/auth/forgot.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
