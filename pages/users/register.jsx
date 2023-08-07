4db4ae26d0bf503907859385a96b5b5d12397d93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 18:10:49 +0100

    fix: add useCallback react hook

--- pages/users/register.jsx
+++ pages/users/register.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
