b4086b4ac6c5c15733f56cc3723997660812cc27
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 17:40:42 +0100

    fix: add ClientOnly components

--- pages/genres/edit/[slug].jsx
+++ pages/genres/edit/[slug].jsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 
 import Meta from '@/components/Meta';
+import ClientOnly from '@/components/ClientOnly';
 import { parseCookie } from '@/utils/index';
 import FormInput from '@/components/FormInput';
 import FormButton from '@/components/FormButton';
