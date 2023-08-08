cfe88dee518b9f9e7fcb5a7b6aeb214ed31c0e44
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 01:18:03 +0100

    fix: refactor

--- pages/auth/account/password.jsx
+++ pages/auth/account/password.jsx
@@ -176,20 +176,10 @@ const UpdatePassword = () => {
                 </div>
               </>
             )}
-
-            <div className={styles.delete}>
-              <h3 className={styles.delete__header}>Close account</h3>
-              <div className={styles.delete__container}>
-                <p className={styles.delete__message}><strong>Warning:</strong> Closing your account is irreversible.</p>
-                <button
-                  onClick={() => setShowModal(false)}
-                  className={styles.delete__btn}
-                  disabled={isLoading}
-                >
-                    Close this account...
-                  </button>
-              </div>
-            </div>
+            <DeactivateAccount
+              disabled={isLoading}
+              onOpen={handleOpen}
+            />
           </div>
         </div>
       </section>
