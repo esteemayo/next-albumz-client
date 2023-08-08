aabe2d77fb8e57d4f975e8c118f99dfb895d616c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 23:03:53 +0100

    fix: import prop-types module

--- components/AlbumForm.jsx
+++ components/AlbumForm.jsx
@@ -1,5 +1,6 @@
 import { toast } from 'react-toastify';
 import { useCallback, useState } from 'react';
+import PropTypes from 'proptypes';
 
 import FormInput from './FormInput';
 import FormTextArea from './FormTextArea';
