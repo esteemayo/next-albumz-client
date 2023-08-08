fe89f9c582db5098e41da131751ed9bd95b0fc61
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 22:17:39 +0100

    fix: fix a bug

--- pages/genres/index.jsx
+++ pages/genres/index.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Link from 'next/link';
-import { useCallback, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Moment from 'react-moment';
 import TagOutlinedIcon from '@mui/icons-material/TagOutlined';
 import PropTypes from 'prop-types';
