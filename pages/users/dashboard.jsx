43d65d5823a882128ce6b258fb983807c59c9862
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 21:54:07 +0100

    fix: refactor

--- pages/users/dashboard.jsx
+++ pages/users/dashboard.jsx
@@ -1,16 +1,43 @@
 import dynamic from 'next/dynamic';
+import { lazy, Suspense, useCallback, useState } from 'react';
 
 import Meta from '@/components/Meta';
 import ClientOnly from '@/components/ClientOnly';
 
-import { parseCookie } from '@/utils/index';
+import { deleteAlbum } from '@/services/albumService';
 import { getUserAlbums } from '@/services/albumService';
+import { deleteGenre } from '@/services/genreService';
+
+import { parseCookie } from '@/utils/index';
 
 import styles from '@/styles/Dashboard.module.scss';
 
 const DashboardCard = dynamic(() => import('@/components/DashboardCard'), { ssr: false });
 
 const Dashboard = ({ albums }) => {
+  const [albumList, setAlbumList] = useState(albums);
+  const [albumId, setAlbumId] = useState(null);
+  const [showModal, setShowModal] = useState(true);
+
+  const handleOpenModal = useCallback((albumId) => {
+    setShowModal(false);
+    setAlbumId(albumId);
+  }, []);
+
+  const handleCloseModal = useCallback((albumId) => {
+    setShowModal(true);
+  }, []);
+
+  const handleDelete = useCallback(() => {
+    try {
+      await deleteAlbum(albumId);
+      setAlbumList((prev) => prev.filter((item) => item._id !== albumId));
+      return toast.success('Album deleted successfully');
+    } catch (err) {
+      console.log(err);
+    }
+  }, [albumId]);
+  
   return (
     <ClientOnly>
       <Meta title='User Dashboard - Albumz Music Entertainment' />
@@ -19,7 +46,13 @@ const Dashboard = ({ albums }) => {
         {albums.length === 0 ? (
           <h2>There are no albums in the database</h2>
         ): (
-          <DashboardCard albums={albums} />
+          <DashboardCard
+            albums={albums}
+            actionId={albumId}
+            onOpen={handleOpenModal}
+            onDelete={handleDelete}
+            onClose={handleCloseModal}
+          />
         )}
       </section>
     </ClientOnly>
