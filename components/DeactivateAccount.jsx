a1ea828da1fb02fbcf9528df98dedd568bdc2175
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 01:13:56 +0100

    feat: create DeactivateAccount component

---
+++ components/DeactivateAccount.jsx
@@ -0,0 +1,19 @@
+const DeactivateAccount = ({ disabled, onOpen }) => {
+  return (
+    <div className={styles.delete}>
+      <h3 className={styles.delete__header}>Close account</h3>
+      <div className={styles.delete__container}>
+        <p className={styles.delete__message}><strong>Warning:</strong> Closing your account is irreversible.</p>
+        <button
+          onClick={onOpen}
+          className={styles.delete__btn}
+          disabled={disabled}
+        >
+            Close this account...
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeactivateAccount;
