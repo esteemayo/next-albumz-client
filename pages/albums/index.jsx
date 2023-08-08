566c724449e0b372b30a1d5c6d071296cebd47b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-08 23:18:40 +0100

    fix: add useCallback react hook

--- pages/albums/index.jsx
+++ pages/albums/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
