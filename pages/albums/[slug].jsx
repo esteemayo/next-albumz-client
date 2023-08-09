ac70bbbc66a352d1d75a484f8203815d48a855c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 14:56:25 +0100

    fix: change file directory

--- pages/albums/[slug].jsx
+++ pages/albums/[slug].jsx
@@ -11,8 +11,8 @@ import styles from '@/styles/SingleAlbum.module.scss';
 const Meta = dynamic(() => import('@/components/Meta'));
 const ReviewForm = dynamic(() => import('@/components/ReviewForm'));
 const Reviews = dynamic(() => import('@/components/Reviews'));
+const SingleAlbumHero = dynamic(() => import('@/components/hero/SingleAlbumHero'));
 const RelatedAlbums = dynamic(() => import('@/components/albums/RelatedAlbums'));
-const SingleAlbumHero = dynamic(() => import('@/components/SingleAlbumHero'));
 const AlbumDescription = dynamic(() => import ('@/components/albums/AlbumDescription'));
 
 const SingleAlbum = ({ album, reviews }) => {
