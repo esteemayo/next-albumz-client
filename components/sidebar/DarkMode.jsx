4a5b1cf4f49a67f25584fa7f7619c13ad32366d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 18:54:24 +0100

    feat: create DarkMode component

---
+++ components/sidebar/DarkMode.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import styles from '@/styles/Sidebar.module.scss';
+
+const DarkMode = ({ icon, onToggle }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.darkmode} onClick={toggleDarkmode}>
+        {icon}
+      </div>
+    </div>
+  );
+};
+
+DarkMode.propTypes = {
+  icon: PropTypes.any.isRequired,
+  onToggle: PropTypes.func.isRequired,
+};
+
+export default DarkMode;
