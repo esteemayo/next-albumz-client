92bb8d16f0a9ac6763347a6babf63e0e57fc3f07
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 17:43:24 +0100

    feat: create MenuButton component

---
+++ components/sidebar/MenuButton.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import styles from '@/styles/Sidebar.module.scss';
+
+const MenuButton = ({ label, onClick }) => {
+  return {
+    <li className={styles.list__items}>
+      <button
+        onClick={onClick}
+        className={styles.btn__logout}
+      >
+        {label}
+      </button>
+    </li>
+  };
+};
+
+MenuButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default MenuButton;
