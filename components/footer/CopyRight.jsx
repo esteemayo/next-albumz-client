a10629b26139282975d36cbeb9595d9569589aa7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 16:51:45 +0100

    feat: create CopyRight component

---
+++ components/footer/CopyRight.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+
+import styles from '@/styles/Footer.module.scss';
+
+const CopyRight = ({ year }) => {
+  return (
+    <p className={styles.footer__copyright}>
+      Copyright &copy; {year} <Link href='/' passHref>Albumz Inc.</Link> All rights reserved
+    </p>
+  );
+};
+
+CopyRight.propTypes = {
+  year: PropTypes.func.isRequired,
+};
+
+export default CopyRight;
