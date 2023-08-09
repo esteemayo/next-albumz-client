22e55985463367314366464d08443a5d05eacbec
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 18:06:09 +0100

    feat: create Search component

---
+++ components/sidebar/Search.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import SearchIcon from '@mui/icons-material/Search';
+
+import styles from '@/styles/Sidebar.module.scss';
+
+const Search = ({ value, onChange, onSubmit }) => {
+  return (
+    <div>
+      <form onSubmit={onSubmit} className={styles.search}>
+        <input
+          type='search'
+          value={value}
+          placeholder='Search albums...'
+          className={styles.search__input}
+          onChange={(value) => onChange(value)}
+        />
+        <SearchIcon className={styles.search__icon} />
+      </form>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Search;
