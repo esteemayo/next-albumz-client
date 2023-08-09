bfdba9fbb2a7add374277678a844e6ac9b0eafbc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 17:31:28 +0100

    feat: create MenuItem component

---
+++ components/sidebar/MenuItem.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+
+import styles from '@/styles/Sidebar.module.scss';
+
+const MenuItem = ({ url, label }) => {
+  return (
+    <Link href={url} passHref>
+      <a className={styles.sidebar__link}>
+         {label}
+      </a>
+    </Link>
+  );
+};
+
+MenuItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default MenuItem;
