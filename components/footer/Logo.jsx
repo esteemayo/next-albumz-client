4d719105759320ff0da31e849dde7a963f28fe1e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 16:30:07 +0100

    feat: create Logo component

---
+++ components/footer/Logo.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Logo = () => {
+  return (
+    <div className={styles.footer__logo}>
+      <Link href='/' passHref>
+        <a className={styles.footer__link}>
+          <Image
+            src='/img/logo-x2.png'
+            width={200}
+            height={70}
+            objectFit='center'
+            alt=''
+          />
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
