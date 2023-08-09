2e3d29390514ef26aaa12c6b7d91881b8e57700a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 17:32:50 +0100

    fix: modify

--- components/sidebar/MenuItem.jsx
+++ components/sidebar/MenuItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import styles from '@/styles/Sidebar.module.scss';
 
-const MenuItem = ({ url, label }) => {
+const MenuItem = ({ url, label, onClose }) => {
   return (
     <Link href={url} passHref>
       <a className={styles.sidebar__link}>
@@ -16,6 +16,7 @@ const MenuItem = ({ url, label }) => {
 MenuItem.propTypes = {
   url: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default MenuItem;
