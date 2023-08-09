96bd3afa7ec63a94079ae85befd05751eb156cce
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 19:05:36 +0100

    fix: add useMemo react hook

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 
