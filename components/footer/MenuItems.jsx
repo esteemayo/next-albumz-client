78b774b2e1bf6988727c4585526d68d172951700
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-09 16:45:26 +0100

    fix: refactor

--- components/footer/MenuItems.jsx
+++ components/footer/MenuItems.jsx
@@ -1,39 +1,23 @@
-import Link from 'next/link';
 import PropTypes from 'prop-types';
+import MenuItem from './MenuItem';
 
 import styles from '@/styles/Footer.module.scss';
 
 const MenuItems = ({ currentUser }) => {
   return (
     <ul className={styles.footer__list}>
-      <li className={styles.footer__item}>
-        <Link href='/albums' passHref>Albums</Link>
-      </li>
+      <MenuItem url='/albums' label='Albums' />
       {currentUser && (
         <>
-          <li className={styles.footer__item}>
-            <Link href='/genres' passHref>Genres</Link>
-          </li>
-          <li className={styles.footer__item}>
-            <Link href='/albums/top' passHref>Top albums</Link>
-          </li>
-          <li className={styles.footer__item}>
-            <Link href='/auth/account' passHref>Account</Link>
-          </li>
-          <li className={styles.footer__item}>
-            <Link href='/users/dashboard' passHref>Dashboard</Link>
-          </li>
+          <MenuItem url='/genres' label='Genres' />
+          <MenuItem url='/albums/top' label='Top albums' />
+          <MenuItem url='/auth/account' label='Account' />
+          <MenuItem url='/users/dashboard' label='Dashboard' />
         </>
       )}
-      <li className={styles.footer__item}>
-        <Link href='#' passHref>Cookie warning</Link>
-      </li>
-      <li className={styles.footer__item}>
-        <Link href='#' passHref>Support</Link>
-      </li>
-      <li className={styles.footer__item}>
-        <Link href='#' passHref>Feedback</Link>
-      </li>
+      <MenuItem url='#' label='Cookie warning' />
+      <MenuItem url='#' label='Support' />
+      <MenuItem url='#' label='Feedback' />
     </ul>
   );
 };
