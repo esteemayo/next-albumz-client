3fba3ca79ab4d8d605ae32ac976d4dfff2601c14
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-10 02:12:00 +0100

    fix: add useMemo react hook

--- components/albums/RelatedAlbumCard.jsx
+++ components/albums/RelatedAlbumCard.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useMemo } from 'react';
 
 import styles from '@/styles/RelatedAlbumCard.module.scss';
 
