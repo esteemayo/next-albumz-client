295516d81f795af77eccffe74db3e16bd60f37c7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-10 01:13:17 +0100

    fix: add useMemo react hook

--- pages/auth/reset/[token].jsx
+++ pages/auth/reset/[token].jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
