014850363bf398a5ce32ebdcae629beb954aa101
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-10 02:58:23 +0100

    fix: add sort to review card items

--- components/review/Reviews.jsx
+++ components/review/Reviews.jsx
@@ -1,7 +1,16 @@
+import { useCallback, useState } from 'react';
+import { ReactSortable } from 'react-sortablejs';
+
 import ReviewCard from './ReviewCard';
 import styles from '@/styles/Reviews.module.scss';
 
 const Reviews = ({ reviews }) => {
+  const [reviewList, setReviewList] = useState(reviews);
+
+  const updateReviewOrder = useCallback((reviews) => {
+    setReviewList(reviews);
+  }, []);
+
   return (
     <>
       {reviews.length > 0 && (
@@ -9,9 +18,11 @@ const Reviews = ({ reviews }) => {
           <h4 className={styles.header}>
             {reviews.length > 1 ? 'Reviews' : 'Review'}
           </h4>
-          {reviews.map((item) => {
-            return <ReviewCard key={item._id} {...item} />;
-          })}
+          <ReactSortable list={reviewList} setList={updateReviewOrder}>
+            {reviewList.map((item) => {
+              return <ReviewCard key={item._id} {...item} />;
+            })}
+          </ReactSortable>
         </section>
       )}
     </>
