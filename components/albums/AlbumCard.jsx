381f3dd304b3ec16dfa4a1bc74f791ff2b562253
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-10 01:41:12 +0100

    fix: add useMemo react hook

--- components/albums/AlbumCard.jsx
+++ components/albums/AlbumCard.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import Link from 'next/link';
+import { useMemo, useState } from 'react';
 import Image from 'next/image';
 
 import { excerpts } from '@/utils/index';
