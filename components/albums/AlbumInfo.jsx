8572d402a17002847b7ee547959679b2577fb0c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-10 15:09:22 +0100

    feat: create AlbumInfo component

---
+++ components/albums/AlbumInfo.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import styles from '@/styles/AlbumDescription.module.scss';
+
+const AlbumInfo = ({ icon: Icon, label, data }) => {
+  return (
+    <div className={styles.album__details}>
+      <span>
+        <Icon className={styles.icon} />
+      </span>
+      <span>{label}</span>
+      <span>{data}</span>
+    </div>
+  );
+};
+
+AlbumInfo.propTypes = {
+  icon: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  data: PropTypes.any.isRequired,
+};
+
+export default AlbumInfo;
