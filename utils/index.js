ccad4f35d95e16721ed8414fffca938c3b486864
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-11 02:56:17 +0100

    fix: add darkModeKey

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,8 @@ import cookie from 'cookie';
 
 export const tokenKey = 'token';
 
+export const darkModeKey = 'dark';
+
 export const excerpts = (str, count) => {
   if (str.length > count) {
     str = str.substring(0, count) + ' ...';
