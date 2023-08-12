236a9d4604bdd2a635e65b5045475de1bd8b6764
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-12 04:52:12 +0100

    fix: modify

--- components/albums/RelatedAlbumCard.jsx
+++ components/albums/RelatedAlbumCard.jsx
@@ -20,7 +20,7 @@ const RelatedAlbumCard = ({
     <div className={styles.related__card}>
       <div className={styles.left}>
         <Image
-          src={image ? image : '/img/default-related-album.webp'}
+          src={image ?? '/img/default-related-album.webp'}
           width='70'
           height='70'
           objectFit='cover'
