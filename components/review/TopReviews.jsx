5142e9650dc7f5528deaf6727ecd3cd2920ac2a5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-13 14:03:30 +0100

    fix: modify

--- components/review/TopReviews.jsx
+++ components/review/TopReviews.jsx
@@ -4,11 +4,13 @@ import styles from '@/styles/TopReviews.module.scss';
 const TopReviews = ({ reviews }) => {
   return (
     <section className={styles.container}>
-      <h3>Testimonials</h3>
       <div className={styles.wrapper}>
-        {reviews?.map((item) => {
-          return <TopReview key={item._id} {...item} />;
-        })}
+        <h3>Testimonials</h3>
+        <div className={styles.box}>
+          {reviews?.map((item) => {
+            return <TopReview key={item._id} {...item} />;
+          })}
+        </div>
       </div>
     </section>
   );
