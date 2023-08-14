ede8d0d605edd39e7c3006550a065a42971c1c8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-14 15:13:57 +0100

    fix: refactor

--- components/button/LikeButton.jsx
+++ components/button/LikeButton.jsx
@@ -16,16 +16,17 @@ const LikeButton = ({ type, likes, actionId, onAction }) => {
     onAction,
   });
 
+  const toggleLike = useCallback(() => {
+    return !user ? null : handleLike;
+  }, [user, handleLike]);
+
   const likeClasses = useMemo(() => {
     return hasFavorited ? 'btnLiked' : 'btnUnlike';
   }, [hasFavorited]);
 
   return (
     <div className={styles.icon__wrapper}>
-      <button
-        onClick={!user ? null : handleLike}
-        className={likeClasses}
-      >
+      <button onClick={toggleLike} className={likeClasses}>
         <HeartButton
           user={user}
           type={type}
