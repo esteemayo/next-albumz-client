66780811ba9a98c9978aa3ff5b3a4927c88964a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 03:02:49 +0100

    fix: add useEffect react hook

--- components/modal/DialogBox.jsx
+++ components/modal/DialogBox.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import PropTypes from 'prop-types';
 import styles from '@/styles/DialogBox.module.scss';
