35d74c899e8c33c6a3934613836888c4cc0813fe
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 15:12:57 +0100

    fix: modify

--- components/account/Sidebar.jsx
+++ components/account/Sidebar.jsx
@@ -1,11 +1,17 @@
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Link from 'next/link';
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Image from 'next/image';
-import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import PropTypes from 'prop-types';
+import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
+import { useRouter } from 'next/router';
 
 const Sidebar = ({ avatar, currentUser, styles }) => {
+  const { pathname } = useRouter();
+
+  const inactiveLink = styles.list__item;
+  const activeLink = `${inactiveLink} ${styles.active}`;
+
   return (
     <div className={styles.left}>
       <div className={styles.userContainer}>
@@ -21,19 +27,19 @@ const Sidebar = ({ avatar, currentUser, styles }) => {
         <h2 className={styles.userName}>{currentUser?.name}</h2>
       </div>
       <ul className={styles.list}>
-        <li className={`${styles.list__item} ${styles.active}`}>
+        <li className={pathname === '/auth/account' ? activeLink : inactiveLink}>
           <HomeOutlinedIcon className={styles.itemIcon} />
           <Link href='/auth/account' passHref>
             <a className={styles.itemLink}>Account</a>
           </Link>
         </li>
-        <li className={styles.list__item}>
+        <li className={pathname.includes('/password') ? activeLink : inactiveLink}>
           <KeyOutlinedIcon className={styles.itemIcon} />
           <Link href='/auth/account/password' passHref>
             <a className={styles.itemLink}>Password</a>
           </Link>
         </li>
-        <li className={styles.list__item}>
+        <li className={pathname.includes('/users/dashboard') ? activeLink : inactiveLink}>
           <DashboardOutlinedIcon className={styles.itemIcon} />
           <Link href='/users/dashboard' passHref>
             <a className={styles.itemLink}>Dashboard</a>
