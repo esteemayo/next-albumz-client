2335a881ba7ea3c0d354b7062f1677dec1e9d1b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 03:03:55 +0100

    fix: modify

--- components/modal/DialogBox.jsx
+++ components/modal/DialogBox.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import { useEffect, useMemo, useState } from 'react';
 
-import PropTypes from 'prop-types';
 import styles from '@/styles/DialogBox.module.scss';
 
 const DialogBox = ({ isOpen, children }) => {
