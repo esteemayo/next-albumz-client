515ec9f5629a33fbbcd39024fdbaf83b4b8f0274
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 15:44:45 +0100

    feat: create MenuItem component

---
+++ components/account/MenuItem.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+
+const MenuItem = ({
+  url,
+  icon: Icon,
+  label,
+  styles,
+  ...rest,
+}) => {
+  return (
+    <li {...rest}>
+      <Icon className={styles.itemIcon} />
+      <Link href={url} passHref>
+        <a className={styles.itemLink}>{label}</a>
+      </Link>
+    </li>
+  );
+};
+
+MenuItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  styles: PropTypes.object.isRequired,
+  rest: PropTypes.any,
+};
+
+export default MenuItem;
