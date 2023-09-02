a2fc74e099892c48f9d0da8f51457b24cc18b7bd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 01:55:53 +0100

    fix: refactor

--- pages/albums/index.jsx
+++ pages/albums/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
