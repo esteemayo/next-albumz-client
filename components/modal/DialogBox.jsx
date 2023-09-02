3a267a4dec972a245557c03b0eb2ddae35b67ea9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-02 02:58:31 +0100

    fix: refactor

--- components/modal/DialogBox.jsx
+++ components/modal/DialogBox.jsx
@@ -3,9 +3,15 @@ import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import styles from '@/styles/DialogBox.module.scss';
 
-const DialogBox = ({ children }) => {
+const DialogBox = ({ isOpen, children }) => {
+  const dialogClasses = useMemo(() => {
+    return isOpen ?
+      `${styles.dialog} ${styles.show}` :
+      {styles.dialog};
+  }, [isOpen]);
+
   return (
-    <section className={styles.dialog}>
+    <section className={dialogClasses}>
       <div className={styles.dialog__container}>
         <div className={styles.dialog__modal}>
           {children}
