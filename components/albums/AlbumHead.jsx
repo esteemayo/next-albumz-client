d520c0f153d29391580b7c50f52c2a4655727704
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-05 01:50:58 +0100

    fix: add FormatNumber component

--- components/albums/AlbumHead.jsx
+++ components/albums/AlbumHead.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useCallback } from 'react';
 
 import LikeButton from '@/components/button/LikeButton';
+import FormatNumber from '@/components/FormatNumber';
 import BookmarkButton from '@/components/button/BookmarkButton';
 
 import StarRating from '../StarRating';
