40fe7adbcb7fc6c77335f7afe2b4e15cac07ca6e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-18 18:32:31 +0100

    feat: create useToggle react custom hook

---
+++ hooks/useToggle.js
@@ -0,0 +1,21 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { closeMenu, toggleMenu } from '@/features/toggle/toggleSlice';
+
+export const useToggle = () => {
+  const dispatch = useDispatch();
+
+  const openMenuHandler = useCallback(() => {
+    dispatch(toggleMenu());
+  }, [dispatch]);
+
+  const closeMenuHandler = useCallback(() => {
+    dispatch(closeMenu());
+  }, [dispatch]);
+
+  return {
+    openMenuHandler,
+    closeMenuHandler,
+  };
+};
